5c5fbaa134a22037c7abfee4284a74e14ea81617
Andresnf6 <andresnf6@hotmail.com>
2024-06-01 09:58:11 -0600

    Commit hecho en clase 15

--- campus/urls.py
+++ campus/urls.py
@@ -1,7 +1,11 @@
+
 from django.contrib import admin
 from django.urls import include, path
+from django.shortcuts import render
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('', lambda request: render(request, "home.html")),
+    path('admin/', admin.site.urls),
     path("attendance/", include("attendance.urls")),
+    path("quizzes/", include("quizzes.urls")),
 ]
